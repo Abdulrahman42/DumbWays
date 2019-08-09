b721c0690e71008940bf0e658ed3a8a52af6755a
Abdulrahman42 <mr33704@gmail.com>
2019-08-09 15:31:40 +0700

    reactapp

---
+++ reactnativeapp/src/IndexApp.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View , Text, StyleSheet, Image} from 'react-native';
+
+
+class IndexApp extends Component{
+    render(){
+        return(
+            <View>
+                <Text>My App maman mm</Text>
+            </View>
+        ); 
+    }
+}
+
+export default IndexApp;
